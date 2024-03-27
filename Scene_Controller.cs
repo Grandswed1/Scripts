e63953dc34d40460bb390e6523a8deb1065806bc
Grandswed1 <elliot.rydell@elev.ga.ntig.se>
2024-03-27 10:22:20 +0100

    idfk

---
+++ Scene_Controller.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Controller : MonoBehaviour
+{
+    public void GotoGame(){
+        SceneManager.LoadScene(0);
+    }
+
+    
+}
